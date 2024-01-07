ac12eca39ca5214cfb0b276e6e499b1c4f18f17d
calebgl <57515964+calebgl@users.noreply.github.com>
2024-01-07 17:40:54 -0600

    add base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { clsx } from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -17,7 +18,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={clsx(
+          inter.className,
+          "grid h-[100svh] min-h-0 gap-2 bg-black p-2 text-white",
+        )}
+      >
+        <aside className="min-h-0 [grid-area:aside]">
+          <nav className="flex h-full flex-col gap-2">
+            <div className="rounded-lg bg-[#121212] px-3 py-2">
+              <ul className="grid items-center">botones</ul>
+            </div>
+            <div className="flex h-full min-h-0 flex-col rounded-lg bg-[#121212] px-3 py-2">
+              <p>aside</p>
+            </div>
+          </nav>
+        </aside>
+        <main className="[grid-area:main]">
+          <div className="h-full rounded-lg bg-[#121212] px-3 py-2">
+            {children}
+          </div>
+        </main>
+        <footer className="[grid-area:player]">
+          <div className="h-24 p-2">player</div>
+        </footer>
+      </body>
     </html>
   );
 }
